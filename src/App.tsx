9aca64ec33c6ae0629f26e52532e13d3495df12e
jarvis5797 <ashutosh.baranwal@acsicorp.com>
2025-09-13 16:49:14 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+         TrainNow Frontend
+      </h1>
+    </div>
+  );
+}
+
+export default App;
